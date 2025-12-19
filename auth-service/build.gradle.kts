9200ee93d923e48a27a41565110b60e41a6de886
신 승민 <shinseungmin070920@gmail.com>
2025-12-19 11:56:50 +0900

    feat(auth): initial setup for authentication microservice

---
+++ auth-service/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
+    id("org.springframework.boot")
+}
+
+description = "auth-service"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.postgresql:postgresql:42.7.8")
+}
